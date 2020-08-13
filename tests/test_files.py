8d7b661d94d58400751ffdfc329c90d5e4f8439c
Michael Erdmann <michaelslab@outlook.de>
2020-08-13 12:33:11 +0200

    gilgamsh epos read from pdf
    testcase done

--- tests/test_files.py
+++ tests/test_files.py
@@ -1,9 +1,16 @@
 import common
+from pdfminer.high_level import extract_text
 
 def filecount(path):
     f = common.getfiles(path)
     return len(f)
 
 def test_delivered():
-    assert filecount("../data")>0
+    assert filecount("../data") > 0
 
+# reading the text file 0
+def test_read_file():
+    f = common.getfiles("../data")
+    text = extract_text( "../data/" +f[0])
+    print(text.split())   ## creating a list of word might be help full later
+    assert len(text.split()) > 1
