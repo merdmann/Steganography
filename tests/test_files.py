e51e366ff0179ab80d68bbecc2ebccf1bdb9de95
Michael Erdmann <michaelslab@outlook.de>
2020-08-15 12:44:19 +0200

    Test is now running success full

--- tests/test_files.py
+++ tests/test_files.py
@@ -15,17 +15,20 @@ def test_delivered():
     assert f[0] == 'Bible - German Luther Translation.pdf'
     assert f[1] == 'The Epic of Gilgamesh.pdf'
 
+# reading file 1 and 0. Both are existing and have different number of
+# words as contents.
 def test_read_file():
     d = read_file(0)
     assert len(d) > 0
     e = read_file(1)
-    assert len(e) > 0
+    assert len(e) >  0 and len(d) > 0
+    assert len(d) != len(e)
 
 # reading the text file 0
 def read_file(id):
     f = common.get_files("../data")
     text = extract_text( "../data/" + f[id])
-    return text.split()   ## creating a list of word might be help full later
     assert len(text.split()) > 1
+    return text.split()
 
 
